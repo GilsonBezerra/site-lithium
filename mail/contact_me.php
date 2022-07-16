95523c96e9c1cfb512ffb5a7ca284bfddef959b7
Gilson <gilson.bezerra@live.com>
2022-07-16 19:42:13 -0300

    Refatorando

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -13,10 +13,10 @@ if(empty($_POST['name'])      ||
       return "index.html";
    }
    
-$name = strip_tags(htmlspecialchars($_POST['name']));
+utf8_decode($name) = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+utf8_decode($message) = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
 $to = 'contato@lithiumproducoes.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
