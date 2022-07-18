1361848f2f0889a71d190fa12b2b8e9a5b24e1b0
Gilson <gilson.bezerra@live.com>
2022-07-18 15:27:31 -0300

    Correção nos campos do formulário

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,7 +12,7 @@ if(empty($_POST['name'])      ||
       return "index.html";
    }
    
-$name = utf8_encode($_POST['name']);
+$name = utf8_decode($_POST['name']);
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
@@ -20,7 +20,7 @@ $message = $_POST['message'];
 // Create the email and send the message
 $to = 'contato@lithiumproducoes.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Contato do site de:  $name";
-$email_body = "Você acaba de receber um novo contato via site Lithium Produções!.\n\n"."Veja os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "Você acaba de receber um novo contato via site Lithium Produções!.\n\n"."Veja os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nTelefone: $phone\n\nMensagem:\n$message";
 $headers = "From: contato@lithiumproducoes.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
