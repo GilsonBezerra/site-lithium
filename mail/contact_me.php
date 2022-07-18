b79a7e39d6901fbd057ab451674120c6ca11ca79
Gilson <gilson.bezerra@live.com>
2022-07-18 10:11:05 -0300

    Correção nos campos do formulário

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,62 +1,29 @@
 <?php
- 
- //Variaveis de POST, Alterar somente se necessário 
- //====================================================
- if(empty($nome = $_POST['nome'])           ||
-    empty($email = $_POST['email'])         ||
-    empty($cidade = $_POST['cidade'])       ||
-    empty($estado = $_POST['estado'])       ||
-    empty($telefone = $_POST['telefone'])   ||
-    empty($mensagem = $_POST['mensagem'])   ||
-    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
-    echo  "<script>
-                window.location='index.html';
-                alert('Todos os campos são de preenchimento obrigatório!');
+ini_set('default_charset','UTF-8');
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)){
+      echo  "<script>
+               window.location='index.html';
+               alert('Todos os campos são de preenchimento obrigatório!');
             </script>"; ;
-    return "index.html";
-}
- //====================================================
- 
- //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
- //==================================================== 
- $email_remetente = "contato@lithiumproducoes.com.br"; // deve ser uma conta de email do seu dominio 
- //====================================================
- 
- //Configurações do email, ajustar conforme necessidade
- //==================================================== 
- $email_destinatario = "contato@lithiumproducoes.com.br"; // pode ser qualquer email que receberá as mensagens
- $email_reply = "$email"; 
- $email_assunto = "Contato do Site"; // Este será o assunto da mensagem
- //====================================================
- 
- //Monta o Corpo da Mensagem
- //====================================================
- $email_conteudo = "Nome = $nome \n"; 
- $email_conteudo .= "Email = $email \n";
- $email_conteudo .= "Cidade = $cidade \n"; 
- $email_conteudo .= "Estado = $estado \n"; 
- $email_conteudo .= "Telefone = $telefone \n"; 
- $email_conteudo .= "Mensagem = $mensagem \n"; 
- //====================================================
- 
- //Seta os Headers (Alterar somente caso necessario) 
- //==================================================== 
- $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Return-Path: $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
- //====================================================
- 
- //Enviando o email 
- //==================================================== 
- if (mail($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
- echo "<script>
-            window.location='index.html';
-            alert('$nome, sua mensagem foi enviada com sucesso! Estaremos retornando em breve');
-        </script>"; 
- 
- } 
- else{ 
- echo "</b>Falha no envio do E-Mail!</b>"; } 
- //====================================================
- 
- ?>
- <a href="./index.html">Clique aqui para voltar ao site</a>
-
+      return "index.html";
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'contato@lithiumproducoes.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contato do site de:  $name";
+$email_body = "Você acaba de receber um novo contato via site Lithium Produções!.\n\n"."Veja os detalhes:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: contato@lithiumproducoes.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
